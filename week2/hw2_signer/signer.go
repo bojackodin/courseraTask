e599c17c890b420f464b5c2f35ae2792f4aa5cf1
balkan <balkan@pop-os.localdomain>
2024-05-13 23:33:32 +0500

    clean

--- week2/hw2_signer/signer.go
+++ week2/hw2_signer/signer.go
@@ -7,7 +7,7 @@ import (
 	"sync"
 )
 
-//  сюда писать код
+// сюда писать код
 func ExecutePipeline(jobs ...job) {
 	var wg sync.WaitGroup
 	in := make(chan interface{})
@@ -52,8 +52,6 @@ func SingleHash(in, out chan interface{}) {
 }
 
 func MultiHash(in, out chan interface{}) {
-	// var mu sync.Mutex
-
 	var wg sync.WaitGroup
 	for v := range in {
 		data := v.(string)
@@ -81,33 +79,6 @@ func MultiHash(in, out chan interface{}) {
 	}
 	wg.Wait()
 }
-
-// func MultiHash(in, out chan interface{}) {
-// 	var mu sync.Mutex
-// 	var wg sync.WaitGroup
-// 	for v := range in {
-// 		data := v.(string)
-// 		fmt.Println(data)
-// 		wg.Add(1)
-// 		var res string
-// 		go func(wg *sync.WaitGroup, data string, res string, mu *sync.Mutex) {
-// 			defer wg.Done()
-// 			for i := 0; i <= 5; i++ {
-// 				wg.Add(1)
-// 				go func(i int) {
-// 					defer wg.Done()
-// 					mu.Lock()
-// 					res += DataSignerCrc32(strconv.Itoa(i) + data)
-// 					fmt.Println(res)
-// 					mu.Unlock()
-// 				}(i)
-// 			}
-// 		}(&wg, data, res, &mu)
-// 		wg.Wait()
-// 		fmt.Println(res)
-// 		out <- res
-// 	}
-// }
 
 func CombineResults(in, out chan interface{}) {
 	s := []string{}
